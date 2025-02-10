81bbe2659e2250dc687e28cfb17370a576fec779
raimu38 <176199024+raimu38@users.noreply.github.com>
2025-02-10 23:29:59 +0900

    mod

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,14 @@
+mod front_of_house {
+    mod hosting [
+        fn add_to_wailist() {}
+        fn seat_at_table() {}
+    }
+    
+    mod serving {
+        fn take_order() {}
+        
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
